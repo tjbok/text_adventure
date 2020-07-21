9e548e8419cf3d64bbdabd6426726b652ddc52c0
Tomas Bok <tomas.j.bok@gmail.com>
2020-07-21 00:36:58 +0000

    Changed rock to a rag doll

--- item_handlers.py
+++ item_handlers.py
@@ -3,13 +3,13 @@
 # To add a new item handler, first create a function for your item
 #  and then "bind" the handler to your item in the bottom section of the file.
 
-def Rock(context, action):
+def Doll(context, action):
     if action["key"] == "ATTACK":
-        context.Print("Wow, you really showed that rock who's boss!")
+        context.Print("Wow, you really showed that doll who's boss!")
         return True
     return False
 
 # Here is where you "bind" your item handler function to a specific item.
 def Register(context):
     items = context.items
-    items.AddItemHandler("ROCK", Rock)
+    items.AddItemHandler("DOLL", Doll)
